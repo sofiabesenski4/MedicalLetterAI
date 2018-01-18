74d09eece5d42f7ced57d7aa6fef03c261ab52f4
thomasbesenski <thomaselijahbesenski@gmail.com>
2018-01-18 15:54:16 -0800

    adding all the folders

---
+++ Resources/Developing a corpus of clinical notes manually annotated for part-of-speech - ScienceDirect_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "VANCOUVER",
+      'continent': "NA",
+      'country': "CA",
+      'region': "BC"
+    },
+    'ip':"70.66.148.96"
+  }]);
+})
+//
+()
+
+;
